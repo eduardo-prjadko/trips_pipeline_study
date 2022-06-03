746894f9d0defe0f5520e9ffbec3b0b36defc86c
Eduardo Prjadko <eduardoabp@gmail.com>
2022-06-02 21:28:01 -0300

    add ouput

--- __main__.py
+++ __main__.py
@@ -44,6 +44,9 @@ trips_primary_key = pulumi.Output.all(resource_group.name, trips_storage.name) \
         account_name=args[1]
     )).apply(lambda accountKeys: accountKeys.keys[0].value)
 
+trips_conn_string = pulumi.Output.all(trips_storage.name, trips_primary_key) \
+    .apply(lambda args: f'DefaultEndpointsProtocol=https;AccountName={args[0]};AccountKey={args[1]};EndpointSuffix=core.windows.net')
+
 # Export the primary key of the Storage Account
 af_primary_key = pulumi.Output.all(resource_group.name, af_storage.name) \
     .apply(lambda args: storage.list_storage_account_keys(
@@ -53,3 +56,4 @@ af_primary_key = pulumi.Output.all(resource_group.name, af_storage.name) \
 
 pulumi.export('trips_primary_storage_key', trips_primary_key)
 pulumi.export('af_primary_storage_key', af_primary_key)
+pulumi.export('trips_conn_string', trips_conn_string)
